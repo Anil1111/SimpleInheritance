3bd87fed6a2c2dae28b1370b64c5a1d215d88a06
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-02-23 20:36:53 +0530

    SimpleInheritance

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace ConsoleApp3
+
+class Tank
 {
-    class Program
+
+    double t_radius;
+    double t_height;
+
+    // Properties for Radius and Height 
+    public double Radius
     {
-        static void Main(string[] args)
+        get
         {
+            return t_radius;
         }
+
+        set
+        {
+            t_radius = value < 0 ? -value : value;
+        }
+    }
+
+    public double Height
+    {
+        get
+        {
+            return t_height;
+        }
+
+        set
+        {
+            t_height = value < 0 ? -value : value;
+        }
+    }
+
+    // Display the dimension of tanks 
+    public void DisplayDimension()
+    {
+        Console.WriteLine("The radius of tank is :" + Radius
+                + " and the height of tank is :" + Height);
+    }
+}
+
+// A derived class AreaOfTank 
+// inheriting Tank Class 
+class AreaOfTank : Tank
+{
+
+    string Color;
+
+    // Constructor 
+    public AreaOfTank(string c, double r, double h)
+    {
+
+        // from base class 
+        Radius = r;
+        Height = h;
+
+        // from derived class 
+        Color = c;
+    }
+
+    // Return area of tank 
+    public double Area()
+    {
+        return 2 * 3.14 * Radius * Height;
+    }
+
+    // Display the color of tank 
+    public void DisplayColor()
+    {
+        Console.WriteLine("The Color of tank is "
+                                        + Color);
+    }
+}
+
+
+class Demo
+{
+
+    // Main Method 
+    static void Main()
+    {
+
+        // Create and initialize the 
+        // object of AreaOfTank 
+        AreaOfTank t1 = new AreaOfTank("Red", 7.0, 1.0);
+        t1.DisplayColor();
+        t1.DisplayDimension();
+        Console.WriteLine("Area is " + t1.Area());
     }
 }
